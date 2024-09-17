6258e39d0cc771f5051374b8646da7c920ab02a6
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-09-17 17:23:47 -0300

    design finalizado

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+//pages
+import Login from './routes/login.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element:<Login/>,
+      }
+    ]
+  }
+])
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      rotate:{
+        '146':'146deg'
+      },
+      width:{
+        
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
