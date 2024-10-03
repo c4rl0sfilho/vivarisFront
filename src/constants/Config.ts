00eff2f6cd1aadaf8e19a5f7dece7b3d4ba8c304
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-10-03 12:14:38 -0300

    avançoes em disponibilidade

---
+++ src/constants/Config.ts
@@ -0,0 +1,3 @@
+export const STORE_OPENING_TIME = 10 // IN HOURS
+export const STORE_CLOSING_TIME = 20 // IN HOURS
+export const INTERVAL = 60 // IN MINUTES
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,71 @@
-
-import React from 'react'
-import HeaderHome from '../components/HeaderHome'
+import React, { useState, useEffect } from 'react';
+import HeaderHome from '../components/HeaderHome';
+import Calendar from 'react-calendar';
+import { add, format } from 'date-fns'; // Corrigido para 'date-fns'
+import '../styles/Calendar.css';
+import { INTERVAL, STORE_CLOSING_TIME, STORE_OPENING_TIME } from '../constants/Config';
 
 const Home = () => {
+  interface dateType {
+    justDate: Date | null;
+    dateTime: Date | null;
+  }
+
+  const [date, setDate] = useState<dateType>({
+    justDate: null,
+    dateTime: null,
+  });
+
+  useEffect(() => {
+    console.log(date.dateTime);
+  }, [date.dateTime]); // Isso vai executar apenas quando dateTime mudar
+  
+  const getTimes = () => {
+    if (!date.justDate) return [];
+
+    const { justDate } = date;
+    const beginning = add(justDate, { hours: STORE_OPENING_TIME });
+    const end = add(justDate, { hours: STORE_CLOSING_TIME });
+    const interval = INTERVAL; // em minutos
+
+    const times = [];
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      times.push(i);
+    }
+
+    return times;
+  };
+
+  const times = getTimes();
+
   return (
     <div>
-      <HeaderHome/>
-      <h1 className='text-9xl'>--- O CONTEUDO VAI AQUI ---</h1>
+      {/* <HeaderHome /> */}
+      <div className='h-screen flex flex-col justify-center items-center'>
+        {date.justDate ? (
+          <div className='flex gap-4'>
+            {times.map((time, i) => (
+              <div key={`time-${i}`} className='rounded-sm bg-gray-100 p-2'>
+                <button
+                  type='button'
+                  onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))} // Corrigido para dateTime
+                >
+                  {format(time, 'HH:mm')} {/* Formato de 24 horas */}
+                </button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <Calendar
+            minDate={new Date()}
+            className='REACT-CALENDAR p-2'
+            view='month'
+            onClickDay={(date) => setDate((prev) => ({ ...prev, justDate: date }))}
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
