3b4a2b3906c0d32fc1b31214b081476a6428b970
CARLOS EDUARDO DA SILVA FILHO <23111777@dsn127m1246727.cfp127aluno.local>
2024-10-22 15:12:01 -0300

    atualizacoes layout login e register

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,29 +2,43 @@
 import whiteSquare from '../assets/whiteSquare.svg'
 import twoBoys from '../assets/twoBoys.svg'
 import ContainerLogin from '../components/ContainerLogin'
-
 import vivarisLogo from '../assets/vivarisLogo.svg'
+import { useMediaQuery } from '@mui/material';
 
 
 
 function login() {
+
+  const isMobile = useMediaQuery(`(max-width: 768px)`)
+
+  console.log('isMobile:', isMobile)
   
   return (
     <div className="w-full h-full flex bg-[#52B693] flex-row fixed">
-      <div className='flex flex-col'>
-        <img src={whiteSquare} alt="" className='absolute pl-[40rem] -z-10' />
-        <div className='flex'>
+      {isMobile ? (
+        <div className='flex flex-col items-center gap-32'>
           <img src={vivarisLogo} alt="" />
-        </div>
-        <h1 className='text-[#ffffff] text-8xl font-semibold pl-[90px] mt-[144px]'>Seja<br />Bem Vindo!</h1>
-        <img src={twoBoys} alt="" className='mt-[5rem] ml-[400px] ' />
-      </div>
-      <div className='flex flex-col p-8 ml-12 justify-center '>
-        <ContainerLogin />
-        {/* <ContainerRegister/> */}
-      </div>
+          <ContainerLogin />
+        </div> // Adicione algum conteúdo aqui
+      ) : (
+        <>
+          <img src={whiteSquare} alt="" className='absolute bottom-0 h-full right-0 -z-10' />
+          <div className='flex flex-col w-1/2'>
+            <div className='flex'>
+              <img src={vivarisLogo} alt="" />
+            </div>
+            <h1 className='text-[#ffffff] text-8xl font-semibold pl-[90px] mt-[144px]'>Seja<br />Bem Vindo!</h1>
+            <img src={twoBoys} alt="" className='mt-[5rem] ml-[400px]' />
+          </div>
+          <div className='flex flex-col p-8 w-1/2 justify-center items-center'>
+            <ContainerLogin />
+            {/* <ContainerRegister/> */}
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
+  
 }
 
 export default login
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -8,20 +8,18 @@ import ContainerRegister from '../components/ContainerRegister';
 
 const Register = () => {
   return (
-    <div>
-      <div className="w-full h-full flex bg-[#52B693] flex-row fixed">
-      <div className='flex flex-col'>
-        <img src={whiteSquare} alt="" className='absolute pl-[40rem] -z-10' />
+    <div className="w-full h-full flex bg-[#52B693] flex-row fixed">
+      <img src={whiteSquare} alt="" className='absolute bottom-0 h-full right-0 -z-10' />
+      <div className='flex flex-col w-1/2'>
         <div className='flex'>
           <img src={vivarisLogo} alt="" />
         </div>
         <h1 className='text-[#ffffff] text-8xl font-semibold pl-[90px] mt-[144px]'>Seja<br />Bem Vindo!</h1>
         <img src={twoBoys} alt="" className='mt-[5rem] ml-[400px] ' />
       </div>
-      <div className='flex flex-col p-8 ml-12 justify-center '>
+      <div className='flex flex-col p-8 w-1/2  justify-center items-center '>
         <ContainerRegister/>
       </div>
-    </div>
     </div>
   )
 }
