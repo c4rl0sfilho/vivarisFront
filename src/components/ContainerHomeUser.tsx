48ef891f8a0620721afcb29a1d5e3aa63148de0f
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-10-24 16:17:04 -0300

    tela home user finalizada

--- src/components/ContainerHomeUser.tsx
+++ src/components/ContainerHomeUser.tsx
@@ -5,8 +5,14 @@ import imgBlog from '../assets/blog.svg';
 import imgGroupConversation from '../assets/groupConversation.svg';
 import { FaPlus } from "react-icons/fa6";
 import imgBell from '../assets/bell.svg';
+import imgChatBot from '../assets/chatBotIcon.svg'
+import imgHumorBalance from '../assets/humorBalanceIcon.svg'
+import imgConsultas from '../assets/consultasIcon.svg'
+import imgMeditate from '../assets/meditate.svg'
 import imgRadioWaves from '../assets/radioWaves.svg';
+import { TiEdit } from "react-icons/ti";
 import { Navigate, useNavigate } from 'react-router-dom';
+import emoji from '../assets/emoji.svg'
 
 
 import { register } from 'swiper/element/bundle';
@@ -40,8 +46,35 @@ const ContainerHomeUser = () => {
                 </div>
                 <div className="w-[100%] h-[100%]  flex flex-row">
                     <div className=" w-1/2 h-[100%] flex flex-col items-end justify-center gap-12">
-                        <div className="graphicHumor h-[20rem] w-[32rem] rounded-2xl bg-[#2AAC88]">
-
+                        <div className="graphicHumor h-[20rem] w-[32rem] rounded-2xl bg-[#2AAC88] py-4">
+                            <div className="title w-full h-1/3 my-4     ">
+                                <h1 className='text-xl md:text-3xl lg:text-4xl font-medium text-center text-[#FFFFFF]'>
+                                    Como você se sente<br /> hoje?
+                                </h1>
+                            </div>
+                            <div className="emotions w-full h-1/3 flex justify-around">
+                                {[1, 2, 3, 4, 5].map((_, index) => (
+                                    <div key={index} className="flex flex-col items-center">
+                                        <img src={emoji} alt={`emoji-${index}`} className="w-12 h-12" />
+                                        <input
+                                            type="checkbox"
+                                            name={`emoji-${index}`}
+                                            id={`emoji-${index}`}
+                                            className="appearance-none w-6 h-6 rounded-full border-2 border-[#60c2a6] checked:bg-[#07503b]  focus:outline-none cursor-pointer"
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="description w-full h-1/3 flex justify-center">
+                                <div className='flex h-12 w-[20rem] rounded-2xl bg-[#1C7D62] items-center border border-white'>
+                                    <TiEdit size={30} color='white' className='ml-2' />
+                                    <input
+                                        type="text"
+                                        placeholder='Gostaria de falar sobre?'
+                                        className='bg-transparent border-none pl-2 placeholder:text-white placeholder:font-normal'
+                                    />
+                                </div>
+                            </div>
                         </div>
                         <div
                             className='slider h-[7rem] w-[30rem] bg-[#15A27A] rounded-2xl mr-8'
@@ -59,7 +92,7 @@ const ContainerHomeUser = () => {
                             >
                                 {data.map((item) => (
                                     <SwiperSlide key={item.id}>
-                                        <div className='w-full h-[60%] flex items-center  py-6 px-4'>
+                                        <div className='w-full h-[60%] flex items-center  py-6 px-4 cursor-pointer'>
                                             <img src={item.image} alt={`Slide ${item.id}`} className="w-16 h-16 object-cover mr-16" />
                                             <h1 className='text-[#F1F1F1] text-2xl font-bold'>{item.nome}</h1>
                                         </div>
@@ -69,10 +102,49 @@ const ContainerHomeUser = () => {
                         </div>
                     </div>
                     <div className=" w-1/2 h-[100%] flex flex-col items-start justify-center gap-12">
-                        <div className="graphicHumor h-[20rem] w-[32rem] rounded-2xl bg-[#2ACC88] ml-24">
+                        <div className="buttons h-[20rem] w-[32rem] rounded-2xl ml-24">
+                            <div className="flex w-[100%] h-1/2  justify-center items-center gap-8">
+                                <div className='buttons rounded-3xl bg-[#CBEBDA] h-24 w-24 sm:h-32 sm:w-32 px-3 sm:px-5 py-3 flex flex-col items-center justify-center cursor-pointer'>
+                                    <img src={imgBlog} alt="Meus Chats" className='w-10 sm:w-16' />
+                                    <p className='text-[#296856] font-bold text-xs sm:text-base text-center leading-none'>Blog</p>
+                                </div>
+                                <div className='buttons rounded-3xl bg-[#CBEBDA] h-24 w-24 sm:h-32 sm:w-32 px-3 sm:px-5 py-3 flex flex-col items-center justify-center cursor-pointer'>
+                                    <img src={imgBook} alt="Meus Chats" className='w-10 sm:w-16' />
+                                    <p className='text-[#296856] font-bold text-xs sm:text-base text-center leading-none'>Diário</p>
+                                </div>
+                                <div className='buttons rounded-3xl bg-[#CBEBDA] h-24 w-24 sm:h-32 sm:w-32 px-3 sm:px-5 py-3 flex flex-col items-center justify-center cursor-pointer'>
+                                    <img src={imgBatePapo} alt="Meus Chats" className='w-10 sm:w-16' />
+                                    <p className='text-[#296856] font-bold text-xs sm:text-base text-center leading-none'>Meus Chat’s</p>
+                                </div>
+                            </div>
+                            <div className="flex w-[100%] h-1/2  justify-center items-center gap-8">
+                                <div className='buttons rounded-3xl bg-[#CBEBDA] h-24 w-24 sm:h-32 sm:w-32 px-3 sm:px-5 py-3 flex flex-col items-center justify-center cursor-pointer'>
+                                    <img src={imgHumorBalance} alt="Meus Chats" className='w-10 sm:w-16' />
+                                    <p className='text-[#296856] font-bold text-xs sm:text-base text-center leading-none'>Gráfico do Humor</p>
+                                </div>
+                                <div className='buttons rounded-3xl bg-[#CBEBDA] h-24 w-24 sm:h-32 sm:w-32 px-3 sm:px-5 py-3 flex flex-col items-center justify-center cursor-pointer'>
+                                    <img src={imgChatBot} alt="Meus Chats" className='w-10 sm:w-16' />
+                                    <p className='text-[#296856] font-bold text-xs sm:text-base text-center leading-none'>ChatBot</p>
+                                </div>
+                                <div className='buttons rounded-3xl bg-[#CBEBDA] h-24 w-24 sm:h-32 sm:w-32 px-3 sm:px-5 py-3 flex flex-col items-center justify-center cursor-pointer'>
+                                    <img src={imgConsultas} alt="Meus Chats" className='w-10 sm:w-16' />
+                                    <p className='text-[#296856] font-bold text-xs sm:text-base text-center leading-none'>Minhas Consultas</p>
+                                </div>
+                            </div>
 
                         </div>
-                        <div className="h-[7rem] w-[80%] bg-[#1C7D62] rounded-2xl"></div>
+                        <div className="h-[7rem] w-[80%] bg-[#1C7D62] rounded-2xl border-4 border-[#1C7D62] flex justify-around items-center cursor-pointer">
+                            <img src={imgMeditate} alt="" />
+                            <h1 className='text-xl md:text-3xl lg:text-4xl font-medium text-[#FFFFFF]'>
+                                Trilhas De Meditação
+                            </h1>
+                            <div className="w-20 h-20 rounded-full flex justify-center items-center bg-gradient-to-r from-[#2DE3C3] to-[#F4B400] p-[3px]">
+                                <div className='h-full w-full flex justify-center items-center rounded-full bg-[#1C7D62]'>
+                                    <p className='text-lg md:text-xl lg:text-3xl font-medium text-white '>25%</p>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
