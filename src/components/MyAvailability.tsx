b8596f601c08b688b75c07e8aca11e01824e409b
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-11-05 17:25:50 -0300

    disponibilidade quase finalizada

--- src/components/MyAvailability.tsx
+++ src/components/MyAvailability.tsx
@@ -1,69 +1,86 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { format, addHours, parseISO } from 'date-fns';
 
-// Mapeamento dos dias da semana
 const diasDaSemana = [
-    { id: 1, dia: 'Segunda-feira' },
-    { id: 2, dia: 'Terça-feira' },
-    { id: 3, dia: 'Quarta-feira' },
-    { id: 4, dia: 'Quinta-feira' },
-    { id: 5, dia: 'Sexta-feira' },
-    { id: 6, dia: 'Sábado' },
+    { id: 1, dia: 'Segunda' },
+    { id: 2, dia: 'Terca' },
+    { id: 3, dia: 'Quarta' },
+    { id: 4, dia: 'Quinta' },
+    { id: 5, dia: 'Sexta' },
+    { id: 6, dia: 'Sabado' },
     { id: 7, dia: 'Domingo' },
 ];
 
 const MyAvailability = ({ reloadAvailability }) => {
     const [data, setData] = useState([]);
 
+    const fetchData = async () => {
+        const idPsicologo = localStorage.getItem('idDoPsicologo');
+        const endPoint = `http://localhost:8080/v1/vivaris/disponibilidade/psicologo/${idPsicologo}`;
+        try {
+            const response = await axios.get(endPoint, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const disponibilidades = response.data.data.disponibilidades;
+
+            const formatHour = (dateString) => {
+                const date = parseISO(dateString);
+                const localDate = addHours(date, 3);
+                return format(localDate, 'HH:mm');
+            };
+
+            const groupedData = {};
+
+            disponibilidades.forEach(item => {
+                const dia = item.dia_semana;
+                const horario = formatHour(item.horario_inicio);
+
+                if (!groupedData[dia]) {
+                    groupedData[dia] = [];
+                }
+
+                groupedData[dia].push({ horario, id: item.id });
+            });
+
+            const formattedData = diasDaSemana.map(({ id, dia }) => {
+                const horarios = groupedData[dia] ? groupedData[dia] : [];
+
+                horarios.sort((a, b) => {
+                    const [hourA, minuteA] = a.horario.split(':').map(Number);
+                    const [hourB, minuteB] = b.horario.split(':').map(Number);
+                    return hourA - hourB || minuteA - minuteB;
+                });
+
+                return { id, dia, horarios };
+            });
+
+            setData(formattedData);
+        } catch (error) {
+            console.error("Erro ao obter dados do usuário:", error);
+        }
+    };
+
+    // Função para deletar um horário de disponibilidade
+    const deleteAvailability = async (id) => {
+        
+        const endPoint = `http://localhost:8080/v1/vivaris/disponibilidade/psicologo/${id}`;
+        try {
+            await axios.delete(endPoint, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            fetchData(); // Recarrega as disponibilidades após a exclusão
+        } catch (error) {
+            console.error("Erro ao deletar a disponibilidade:", error);
+        }
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            const idPsicologo = localStorage.getItem('idDoPsicologo');
-            const endPoint = `http://localhost:8080/v1/vivaris/disponibilidade/psicologo/${idPsicologo}`;
-            try {
-                const response = await axios.get(endPoint, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-
-                const disponibilidades = response.data.data.disponibilidades;
-
-                // Função para formatar horário para "HH:mm"
-                const formatHour = (dateString) => {
-                    const date = new Date(dateString);
-                    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-                };
-
-            
-
-                // Agrupar por dia, remover duplicados e ordenar horários
-                const groupedData = diasDaSemana.map(({ id, dia }) => {
-                    const horarios = disponibilidades
-                        .filter(item => item.dia_semana === dia) // Filtra as disponibilidades para o dia atual
-                        .map(item => formatHour(item.horario_inicio)) // Formata os horários de início
-                        .filter((value, index, self) => self.indexOf(value) === index); // Remove duplicatas
-                
-                    // Ordena os horários em ordem crescente (horas e minutos)
-                    horarios.sort((a, b) => {
-                        const [hourA, minuteA] = a.split(':').map(Number); // Converte a hora e minuto de a
-                        const [hourB, minuteB] = b.split(':').map(Number); // Converte a hora e minuto de b
-                        return hourA - hourB || minuteA - minuteB; // Compara as horas, e depois os minutos
-                    });
-                
-                    return {
-                        id,
-                        dia,
-                        horarios,
-                    };
-                });
-
-                // Atualiza o estado com os dados agrupados
-                setData(groupedData);
-            } catch (error) {
-                console.error("Erro ao obter dados do usuário:", error);
-            }
-        };
-
         fetchData();
     }, [reloadAvailability]);
 
@@ -76,8 +93,12 @@ const MyAvailability = ({ reloadAvailability }) => {
                             <h1 className="font-bold text-center text-[#296856] pb-4">{dia}</h1>
                             <div className="flex flex-col items-center">
                                 {horarios.length > 0 ? (
-                                    horarios.map((horario, index) => (
-                                        <div key={index} className="flex justify-center w-24 p-2 font-medium border-2 rounded-ss-xl rounded-br-xl text-[#3E9C81] border-[#3E9C81] hover:bg-red-600 hover:text-white hover:border-black cursor-pointer transition my-1">
+                                    horarios.map(({ horario, id: horarioId }) => (
+                                        <div
+                                            key={horarioId}
+                                            onClick={() => deleteAvailability(horarioId)}
+                                            className="flex justify-center w-24 p-2 font-medium border-2 rounded-ss-xl rounded-br-xl text-[#3E9C81] border-[#3E9C81] hover:bg-red-600 hover:text-white hover:border-black cursor-pointer transition my-1"
+                                        >
                                             {horario}
                                         </div>
                                     ))
