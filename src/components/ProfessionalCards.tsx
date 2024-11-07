743cb0e6fd7f99807743de050b99f3435ba079d8
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-11-07 17:20:58 -0300

    ajuecw

---
+++ src/components/ProfessionalCards.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfessionalCards = () => {
+  return (
+    <div>
+        <div className="cards">
+            <div className="card"></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default ProfessionalCards
---
+++ src/routes/Nave.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import ProfessionalCards from '../components/ProfessionalCards'
+
+const Nave = () => {
+  return (
+    <div>
+        <ProfessionalCards/>
+    </div>
+  )
+}
+
+export default Nave
